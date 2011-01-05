4a7e637b46e57dfaa762e87d608195b0e6b5e704
Rodrigo Urubatan Ferreira Jardim <rodrigo@urubatan.com.br>
2011-01-04 23:58:27 -0200

    added support for file types other than ruby

--- lib/codemerger/rake_tasks.rb
+++ lib/codemerger/rake_tasks.rb
@@ -18,14 +18,25 @@ namespace "codemerger" do
     in_files.sort.each do |file|
       in_lines = IO.readlines(file).to_s
       in_lines = Maruku.new(in_lines).to_html if file =~ /markdown$/
-      out_f << in_lines.gsub(/(\{\{[\/a-zA-Z0-9]+\.[a-zA-Z0-9]{2,5}\}\})/) do |f_name|
+        out_f << in_lines.gsub(/(\{\{[\/a-zA-Z0-9]+\.[a-zA-Z0-9]{2,5}\}\})/) do |f_name_match|
+        f_name = f_name_match[2..-3]
+        ext = f_name[/(\.[a-zA-Z]+)/]
+        lang_str = case ext
+                   when ".yml"; "lang=\"yaml\""
+                   when ".xml"; "lang=\"xml\""
+                   when ".html"; "lang=\"xml\""
+                   when ".rb"; "lang=\"ruby\""
+                   when nil; "lang=\"ruby\""
+                   else; ""
+                   end
+
         <<_EOF_
-<b>#{f_name[2..-3]}</b>
-<pre line="1" lang="ruby">
-#{IO.readlines(f_name[2..-3]).to_s}
+<b>#{f_name}</b>
+<pre line="1" #{lang_str}>
+        #{IO.readlines(f_name).to_s}
 </pre>
 _EOF_
-      end
+        end
     end
   end
 end
