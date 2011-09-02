205c804ef704ba849ccddce1f57522af48647247
Rodrigo Urubatan <rodrigo@urubatan.com.br>
2011-09-02 01:10:16 -0300

    changes to support markdown and code coloring, thinking about merging code coloring and stop using wordpress coloring

--- lib/codemerger/merger.rb
+++ lib/codemerger/merger.rb
@@ -5,7 +5,11 @@ require 'albino'
 module Codemerger
   class HTMLwithAlbino < Redcarpet::Render::HTML
     def block_code(code, language)
-      Albino.colorize(code, language)
+      if language
+        Albino.colorize(code, language)
+      else
+        %Q{<pre><code>#{code}</code></pre>}
+      end
     end
   end
   class Merger
@@ -19,50 +23,68 @@ module Codemerger
     end
     def get_language_str(ext)
       case ext
-      when ".yml"; "lang=\"yaml\""
-      when ".xml"; "lang=\"xml\""
-      when ".html"; "lang=\"xml\""
-      when ".rb"; "lang=\"ruby\""
-      when ".java"; "lang=\"java\""
-      when ".scala"; "lang=\"scala\""
-      when ".erb"; "lang=\"ruby\""
-      when ".xsl"; "lang=\"xml\""
-      when ".css"; "lang=\"css\""
-      when ".js"; "lang=\"javascript\""
-      when ".sh"; "lang=\"bash\""
-      when ".bat"; "lang=\"batch\""
-      when ".xhtml"; "lang=\"xml\""
-      when nil; "lang=\"ruby\""
-      else; "lang=\"text\""
+      when ".yml"; "yaml"
+      when ".xml"; "xml"
+      when ".html"; "xml"
+      when ".rb"; "ruby"
+      when ".java"; "java"
+      when ".scala"; "scala"
+      when ".erb"; "ruby"
+      when ".xsl"; "xml"
+      when ".css"; "css"
+      when ".js"; "javascript"
+      when ".sh"; "bash"
+      when ".bat"; "batch"
+      when ".xhtml"; "xml"
+      when nil; "ruby"
+      else; "text"
       end
     end
-    def build_merged_file_content(f_name)
+    def build_html_merged_file_content(f_name)
       ext = f_name[/(\.[a-zA-Z]+)/]
       lang_str = get_language_str(ext)
       %Q{
       <b>#{f_name}</b>
-      <pre line="1" #{lang_str}>
-      #{IO.readlines(f_name).join('')}
+      <pre line="1" lang="#{lang_str}">
+        #{IO.readlines(f_name).join('')}
       </pre>
       }
     end
+    def build_md_merged_file_content(f_name)
+      ext = f_name[/(\.[a-zA-Z]+)/]
+      lang_str = get_language_str(ext)
+      %Q{_#{f_name}_
+
+```#{lang_str}
+#{IO.readlines(f_name).join('')}
+```
+      }
+    end
     def process_files
-      in_files = Dir.glob("#{@dir_name}/*.{markdown,html}")
+      in_files = Dir.glob("#{@dir_name}/*.{markdown,md,html}")
       in_files.sort.each do |file|
         in_lines = IO.readlines(file).join('')
-        in_lines = processMarkdown(in_lines) if file =~ /markdown$/
+        is_markdown = (file =~ /markdown$/) || (file =~ /md$/)
         out_fname = file[/^.*\./]
         out_f    = File.new("output/#{out_fname}html", 'w')
-        out_f << in_lines.gsub(/(\{\{[\/a-zA-Z0-9_]+(?>\.[a-zA-Z0-9]{2,}){0,3}\}\})/) do |f_name_match|
-          f_name = f_name_match[2..-3]
-          build_merged_file_content(f_name)
+        if is_markdown
+          in_lines.gsub!(/(\{\{[\/a-zA-Z0-9_]+(?>\.[a-zA-Z0-9]{2,}){0,3}\}\})/) do |f_name_match|
+            f_name = f_name_match[2..-3]
+            build_md_merged_file_content(f_name)
+          end
+          out_f << processMarkdown(in_lines)
+        else
+          out_f << in_lines.gsub(/(\{\{[\/a-zA-Z0-9_]+(?>\.[a-zA-Z0-9]{2,}){0,3}\}\})/) do |f_name_match|
+            f_name = f_name_match[2..-3]
+            build_html_merged_file_content(f_name)
+          end
         end
       end
     end
     def processMarkdown(text)
       @markdown ||= Redcarpet::Markdown.new(HTMLwithAlbino.new,
-      :autolink => true, :space_after_headers => true, :superscript => true,
-      :fenced_code_blocks => true, :tables => true, :no_intra_emphasis => true)
+                                            :autolink => true, :space_after_headers => true, :superscript => true,
+                                            :fenced_code_blocks => true, :tables => true, :no_intra_emphasis => true)
       result = @markdown.render(text)
       result
     end
