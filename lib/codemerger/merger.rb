e1add6c244a7835f5c10d17b892d63e4bccafb3d
Rodrigo Urubatan <rodrigo@urubatan.com.br>
2011-09-15 18:47:13 -0300

    process markdown inside DIV blocks

--- lib/codemerger/merger.rb
+++ lib/codemerger/merger.rb
@@ -7,20 +7,32 @@ module Codemerger
     include Redcarpet::Render::SmartyPants
     def code(code, language)
       if language
-        Albino.colorize(code, language)
+        Albino.new(code, language).colorize({O:"linenos=table,encoding=utf-8"})
       else
         %Q{<pre><code>#{code}</code></pre>}
       end
     end
+    def block_html(raw_html)
+      @markdown ||= Redcarpet::Markdown.new(HTMLwithAlbino.new,
+                                            :autolink => true, :space_after_headers => true, :superscript => true,
+                                            :fenced_code_blocks => true, :tables => true, :no_intra_emphasis => true)
+      m,tag,attrs,content = *(raw_html.match(/<([a-z]+)(.*?)>(.*)<\/\1>/m))
+      result = @markdown.render(content)
+      %Q{<#{tag}#{attrs}>
+         #{result}
+        </#{tag}>
+      }
+    end
     def block_code(code, language)
       if language
-        Albino.colorize(code, language)
+        Albino.new(code, language).colorize({O:"linenos=table,encoding=utf-8"})
       else
         %Q{<pre><code>#{code}</code></pre>}
       end
     end
   end
   class Merger
+    attr_reader :markdown
     def initialize(dir_name)
       @dir_name = dir_name
     end
@@ -87,12 +99,14 @@ module Codemerger
         out_f    = File.new("output/#{out_fname}html", 'w')
         if is_markdown
           in_lines.gsub!(/(\{\{[\/a-zA-Z0-9:_]+(?>\.[a-z:_A-Z0-9]{2,}){0,3}\}\})/) do |f_name_match|
+            puts "Merging #{f_name_match}"
             f_name = f_name_match[2..-3]
             build_md_merged_file_content(f_name)
           end
           out_f << processMarkdown(in_lines)
         else
           out_f << in_lines.gsub(/(\{\{[\/a-zA-Z0-9_]+(?>\.[a-zA-Z0-9]{2,}){0,3}\}\})/) do |f_name_match|
+            puts "Merging #{f_name_match}"
             f_name = f_name_match[2..-3]
             build_html_merged_file_content(f_name)
           end
