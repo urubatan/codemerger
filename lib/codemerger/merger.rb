f9301e5efdbd1ba468663c79284cb8fa29959511
Rodrigo Urubatan <rodrigo@urubatan.com.br>
2013-06-03 02:48:42 -0300

    other test

--- lib/codemerger/merger.rb
+++ lib/codemerger/merger.rb
@@ -128,6 +128,7 @@ module Codemerger
       in_files = Dir.glob("#{@dir_name}/**/*.{markdown,md,html}")
       in_files.sort.each do |file|
         in_lines = IO.readlines(file).join('')
+        puts "#{file} - #{file.size}"
         is_markdown = (file =~ /markdown$/) || (file =~ /md$/)
         out_fname = file[/^.*\./]
         out_ext = "html"
@@ -138,7 +139,7 @@ module Codemerger
         out_f    = File.new(out_f_name, 'w')
         if is_markdown
           out_f << in_lines.gsub!(/(\{\{[\/a-zA-Z0-9:_]+(?>\.[a-z:_A-Z0-9]{2,}){0,3}\}\})/) do |f_name_match|
-            puts "Merging #{f_name_match}"
+            #puts "Merging #{f_name_match}"
             f_name = f_name_match[2..-3]
             build_md_merged_file_content(f_name)
           end
