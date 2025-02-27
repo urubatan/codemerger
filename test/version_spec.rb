b6fccb72121daf7d5da2503e6786ae2ad6bbcdc3
Rodrigo Urubatan Ferreira Jardim <rodrigo.jardim@donorbox.org>
2025-02-27 10:40:22 -0300

    remove unneeded version check

--- test/version_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'test_helper'
-require "codemerger/version"
-
-describe Codemerger do
-  it "should return the correct version number" do
-    _("0.5.0").must_equal Codemerger::VERSION
-  end
-end
